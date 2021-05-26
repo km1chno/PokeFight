c7846501d21bc76807652d44ae992a5e2d722359
GnFC <47872060+GnFC@users.noreply.github.com>
2021-05-26 11:39:41 +0200

    Improved name search and basic pagination

--- src/sample/controllers/LibraryViewController.java
+++ src/sample/controllers/LibraryViewController.java
@@ -1,14 +1,13 @@
 package sample.controllers;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import sample.components.LibraryFilterBarControl;
 import sample.components.SinglePokemonTypeTileControl;
 import sample.model.Utils;
@@ -17,30 +16,57 @@ import sample.model.providers.PokemonTypeListFilter;
 import sample.model.providers.PokemonTypeListProvider;
 
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.function.BiFunction;
 
 public class LibraryViewController implements Initializable {
-    private int NUMBER_OF_POKEMONS;
-    private PokemonTypeList pokemonList;
-    private LibraryFilterBarControl filterBar;
+    private int NUMBER_OF_POKEMONS; // The number of all the fetched pokemons
+    private int NUMBER_TO_LOAD; // The number of all pokemons displayed on screen
+    private PokemonTypeList pokemonList; // List containing all of the fetched pokemons
+    private LibraryFilterBarControl filterBar; // Bar object responsible for user interaction with filtering
+    private int loadedPosition; // Index of last element displayed by tilePane in pokemonList
 
     @FXML
-    ScrollPane scrollPane;
+    ScrollPane scrollPane; // Container allowing for scrolling of content
 
     @FXML
-    TilePane tilePane;
+    TilePane tilePane; // Container for PokemonTiles
 
     @FXML
     AnchorPane anchorPane;
 
+    private final ChangeListener<Number> onScroll = (observableValue, number, t1) -> {
+        System.out.println(number + " " + t1);
+        double vmid = (scrollPane.getVmin() + scrollPane.getVmax()) / 2;
+        if ((double)t1 == 0 && (double)number == vmid) {
+            scrollPane.setVvalue(0.5);
+            return;
+        }
+        if ((double)t1 >= scrollPane.getVmax()) {
+            if (loadedPosition == NUMBER_OF_POKEMONS) return;
+            loadedPosition = Math.min(loadedPosition + NUMBER_TO_LOAD / 2 - 2, NUMBER_OF_POKEMONS);
+            updateTiles();
+            scrollPane.layout();
+            scrollPane.setVvalue(vmid);
+        }
+        else if ((double)t1 <= scrollPane.getVmin()) {
+            if (loadedPosition == NUMBER_TO_LOAD) return;
+            loadedPosition = Math.max(loadedPosition - NUMBER_TO_LOAD / 2 + 2, NUMBER_TO_LOAD);
+            updateTiles();
+            scrollPane.layout();
+            scrollPane.setVvalue(vmid);
+        }
+    };
+
+
+    // Updates pokemonList with given filter and applies changes to display
     private final BiFunction<Integer, PokemonTypeListFilter, Void> updateLibraryWithFilter = (count, filter) -> {
         pokemonList = PokemonTypeListProvider.getDataWithFilter(count, filter);
         updateLibrary();
         return null;
     };
 
+    // Initializes the LibraryView
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         filterBar = new LibraryFilterBarControl();
@@ -50,13 +76,27 @@ public class LibraryViewController implements Initializable {
 
         anchorPane.getChildren().add(filterBar);
         pokemonList = PokemonTypeListProvider.getData();
+
         updateLibrary();
     }
 
+    // Updates NUMBER_OF_POKEMONS, NUMBER_TO_LOAD and tilePane based on pokemonList
     void updateLibrary() {
-        tilePane.getChildren().clear();
+        scrollPane.vvalueProperty().removeListener(onScroll);
+
         NUMBER_OF_POKEMONS = pokemonList.getCount();
-        for (int i = 0; i < NUMBER_OF_POKEMONS; i++)
+        NUMBER_TO_LOAD = Math.min(20, NUMBER_OF_POKEMONS);
+        loadedPosition = NUMBER_TO_LOAD;
+        updateTiles();
+        scrollPane.setVvalue(scrollPane.getVmin());
+        scrollPane.vvalueProperty().addListener(onScroll);
+    }
+
+    // Updates PokemonTiles to be displayed on screen by tilePane based on position
+    void updateTiles() {
+        tilePane.getChildren().clear();
+        for (int i = loadedPosition - NUMBER_TO_LOAD; i < loadedPosition; i++)
             tilePane.getChildren().add(new SinglePokemonTypeTileControl(pokemonList.getId(i), pokemonList.getName(i), pokemonList.getUrl(i)));
+        scrollPane.layoutYProperty();
     }
 }
--- src/sample/model/providers/PokemonTypeListFilter.java
+++ src/sample/model/providers/PokemonTypeListFilter.java
@@ -2,20 +2,46 @@ package sample.model.providers;
 
 import sample.model.datamodels.Result;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class PokemonTypeListFilter {
     public String nameSearch = "";
 
     public PokemonTypeListFilter(String nameSearch) {
-        this.nameSearch = nameSearch;
+        this.nameSearch = nameSearch.toLowerCase();
+    }
+
+    private int levenshteinDistance(Result pokemon) {
+        int n = nameSearch.length(), m = pokemon.name.length();
+        if (n > 100) return Integer.MAX_VALUE;
+        if (n == 0) return m;
+        if (m == 0) return n;
+
+        Integer[][] partial = new Integer[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+            partial[i][0] = i;
+        for (int i = 1; i <= m; i++)
+            partial[0][i] = i;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int sameChar = nameSearch.charAt(i - 1) == pokemon.name.charAt(j - 1) ? 0 : 1;
+                partial[i][j] = Integer.min(Integer.min(partial[i - 1][j], partial[i][j - 1]) + 1, partial[i - 1][j - 1] + sameChar);
+            }
+        }
+        System.out.println(pokemon.name + "'s distance is " + partial[n][m]);
+        return partial[n][m];
     }
 
     public boolean test(Result pokemon) {
-        if (nameSearch.length() > pokemon.name.length())
+        /*if (nameSearch.length() > pokemon.name.length())
             return false;
         for (int i = 0; i < nameSearch.length(); i++) {
             if (Character.toLowerCase(nameSearch.charAt(i)) != Character.toLowerCase(pokemon.name.charAt(i)))
                 return false;
         }
-        return true;
+        return true;*/
+        if (Objects.equals("", nameSearch)) return true;
+        return 2 * levenshteinDistance(pokemon) <= pokemon.name.length();
     }
 }
