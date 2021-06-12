3dff6df0a39779b95390544a4c48614cb93653f3
przyjacielpkp <jakubsiuta1616@gmail.com>
2021-06-12 21:46:20 +0200

    Add copy constructors for inheritance logic

---
+++ src/sample/model/fight/SimulatedPokemon.java
@@ -0,0 +1,9 @@
+package sample.model.fight;
+
+import sample.model.datamodels.PokemonInstance;
+
+public class SimulatedPokemon extends PokemonInstance {
+    SimulatedPokemon(PokemonInstance pokemon){
+        super(pokemon);
+    }
+}
